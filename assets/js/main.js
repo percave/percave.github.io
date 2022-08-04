8f27d112f40cab1e074df369a4cf5799404ad089
percave <83913230+percave@users.noreply.github.com>
2022-08-04 11:35:46 +0300

    Contact Form

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,48 @@ function navigate(target)
 
     const target_tab = document.getElementById(target);
     target_tab.classList.add('active-tab');
-
-    history.pushState({}, '', target);
 }
+
+document.getElementById('contact-form').addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const submit_btn = document.getElementById('submit-btn');
+    submit_btn.disabled = true;
+    submit_btn.innerHTML = "<i class='fas fa-spinner fa-spin'></i> Sending...";
+
+    fetch(
+        this.action,
+        {
+            method: this.method,
+            body: new FormData(this),
+            headers: {
+                accept: 'application/json'
+            }
+        }
+    ).then(function(resp) {
+
+        const status = document.getElementById('status');
+
+        if (resp.ok)
+        {
+            status.innerHTML = "Thanks for your submission!";
+            status.classList.remove();
+            status.classList.add('error-success');
+        }
+        else
+        {
+            status.classList.remove();
+            status.classList.add('error-danger');
+            resp.json().then(data => {
+                if (Object.hasOwn(data, 'errors')) {
+                    status.innerHTML = "<b>Error!</b> " + data["errors"].map(error => error["message"]).join(", ")
+                } else {
+                    status.innerHTML = "Oops! There was a problem submitting your form"
+                }
+            })
+        }
+
+        submit_btn.disabled = false;
+        submit_btn.innerHTML = "Send";
+    });
+});
